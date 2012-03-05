152daefdb9cbd3641fb1f353e59bea71e61845b9
Sven Lehnert <mail@sven-lehnert.de>
2012-03-05 20:09:17 +0700

    git sport ;)

---
+++ core/loader.php
@@ -0,0 +1,19 @@
+<?php
+
+define( 'CC_VERSION', '1.8.5' );
+
+/**
+ * loads custom community files
+ *
+ * @package Custom Community
+ * @since 2.0
+ */
+
+function cc_init() {
+	require( dirname( __FILE__ ) . '/custom-community.php' );
+	$cc = new Custom_Community;
+	
+}
+add_action( 'after_setup_theme', 'cc_init', 1, 1 );
+
+?>
