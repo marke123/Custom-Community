f4b4ccc0bbe664f9d04fc71c0b7636ccaa414a9c
Sven Lehnert <mail@sven-lehnert.de>
2012-04-10 15:04:44 +0700

    add new class to home widget

--- index.php
+++ index.php
@@ -19,15 +19,15 @@
 		<?php } else {
 			echo '<div class="home_widgets_container">';
  			for ($ln = 1; $ln <= $tkf->home_widgets_lines_number; $ln++ ){ 
- 				echo '<div class="home_widget_line line_' . $ln . '">';
+ 				echo '<div class="home_widget_line line_' . $ln . ' widgets_amount_'.$tkf->home_widgets_lines_number.'">';
  				
  				for ($wn = 1; $wn <= $tkf->home_widgets_line_widgets_number[$ln]; $wn++ ){
 						
 					if( ! dynamic_sidebar( 'home_widget_line_'.$ln.'_widget_'.$wn )) : ?>
 							<div class="widget">
-								<h3 class="widgettitle" ><?php _e('Widgetaria', 'cc'); ?></h3>
+								<h3 class="widgettitle" ><?php _e('Widgetarea', 'cc'); ?></h3>
 								<div>
-									<p><?php echo 'home_widget_line_'.$ln.'widget_'.$wn ?></p>
+									<p>Add your widgets</p>
 								</div>
 							
 							</div>
